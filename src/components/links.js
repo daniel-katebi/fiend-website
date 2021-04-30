007a1a2e176750befd40a181500c6b7618bb044d
Daniel Katebi <daniel.katebi1@gmail.com>
2021-04-29 22:48:21 -0600

    More Styling2

--- src/components/links.js
+++ src/components/links.js
@@ -17,22 +17,22 @@ function Links() {
       </a>
       <a
         className="Link-text"
-        href="https://discord.com/invite/GZtuygu4gJ"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <GlitchText>
-          <GlitchSquiggly onHover={true}>DISCORD</GlitchSquiggly>
-        </GlitchText>
-      </a>
-      <a
-        className="Link-text"
         href="https://fiendworldwide.bandcamp.com/releases"
         target="_blank"
         rel="noopener noreferrer"
       >
         <GlitchText>
           <GlitchSquiggly onHover={true}>BANDCAMP</GlitchSquiggly>
+        </GlitchText>
+      </a>
+      <a
+        className="Link-text"
+        href="https://fiend-club.creator-spring.com/listing/katebi-cutspace-paq-tee?product=369"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GlitchText>
+          <GlitchSquiggly onHover={true}>Merch</GlitchSquiggly>
         </GlitchText>
       </a>
     </div>
